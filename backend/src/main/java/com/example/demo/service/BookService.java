fa11ab7a00bb315f1510799b5e86a1f92f32698d
Andrii <wertuha22@gmail.com>
2020-10-26 11:10:22 +0100

    Modify controller and add test for it

--- backend/src/main/java/com/example/demo/service/BookService.java
+++ backend/src/main/java/com/example/demo/service/BookService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,6 +24,9 @@ public class BookService {
     public List<Book> findAllBooks() {
     	return bookRepository.findAll();
     }
+    public Optional<Book> findBookById(long id) {
+    	return bookRepository.findById(id);
+    }
     
     public Book createBook(Book book) {
     	return bookRepository.save(book);
@@ -38,6 +42,10 @@ public class BookService {
     
     public void deleteBookById(Long id) {
     	bookRepository.deleteById(id);
+    }
+    
+    public List<Book> findByTitle(String name) {
+    	return bookRepository.findBooksByTitle(name);
     }
 	
 }
--- backend/src/test/java/com/example/demo/controller/BookControllerTest.java
+++ backend/src/test/java/com/example/demo/controller/BookControllerTest.java
@@ -1,100 +1,76 @@
 package com.example.demo.controller;
 
-
-
-import com.example.demo.entity.Book;
-import com.example.demo.repository.BookRepository;
-import com.example.demo.service.BookService;
-
-import org.assertj.core.api.Condition;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assumptions.assumingThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-@ExtendWith(MockitoExtension.class)
+import com.example.demo.entity.Book;
+import com.example.demo.service.BookService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.zalando.problem.ProblemModule;
+import org.zalando.problem.violations.ConstraintViolationProblemModule;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+
+
+@WebMvcTest(controllers = BookController.class)
 public class BookControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
     
-	@Mock                         
-    private BookRepository bookRepository; 
-   
-	@InjectMocks
-	private BookService bookService;
-	
-	@Test
-	public void shouldReturnAll() {
-		List<Book> datas = new ArrayList<>();
-		datas.add(new Book(1L,"Robinson Crusoe", "Book about man on island", 2, null, null));
-		datas.add(new Book(2L,"Robinson Crusoe", "Book about man on island", 5,null, null));
-		datas.add(new Book(3L,"Robinson Crusoe", "Book about man on island", 3,null, null));
-        given(bookRepository.findAll()).willReturn(datas);
+    @MockBean
+    private BookService bookService;
+    
+    @Autowired
+    private ObjectMapper objectMapper;
+    
+    private List<Book> bookList;
+    
+    @BeforeEach
+    void setUp() {
+        this.bookList = new ArrayList<>();
+        bookList.add(new Book(1L,"Robinson Crusoe", "Book about man on island", 2, null, null));
+        bookList.add(new Book(2L,"Tom sawyer", "Book about young boy and his friends", 5,null, null));
+        bookList.add(new Book(3L,"Robinson Crusoe", "Book about man on island", 3,null, null));
+
+        objectMapper.registerModule(new ProblemModule());
+        objectMapper.registerModule(new ConstraintViolationProblemModule());
+    }
+    
+    @Test
+    void shouldRetrieveAllUsers() throws Exception {
+    	given(bookService.findAllBooks()).willReturn(bookList);
+    	
+    	this.mockMvc.perform(get("/api/books/"))
+    		.andExpect(status().isOk())
+    		.andExpect(jsonPath("$.size()", is(bookList.size())));;
+    }
+    
+    @Test
+    void shouldFindBookById() throws Exception {
+        final Long bookId = 1L;
+        final Book book = new Book(3L,"Robinson Crusoe", "Book about man on island", 3,null, null);
         
-        List<Book> expected = bookService.findAllBooks();
+        given(bookService.findBookById(bookId)).willReturn(Optional.of(book));
         
-        assertEquals(expected, datas);
-        
-	}
-	
-	@Test
-	public void shouldSaveSuccesfullyOne() {
-		Book book = new Book(1L,"Robinson Crusoe", "Book about man on island", 5, null, null);
-		given(bookRepository.save(book)).willAnswer(answer->answer.getArgument(0));
-        Book savedBook = bookService.createBook(book);
-        
-        assertThat(savedBook).isNotNull();
-        
-        verify(bookRepository).save(any(Book.class));
-		
-	}
-	
-	@Test
-	public void shouldSaveSuccesfullyList() {
-		List<Book> datas = new ArrayList<>();
-		int expected=4;
-		datas.add(new Book(1L,"Robinson Crusoe", "Book about man on island", 2, null, null));
-		datas.add(new Book(2L,"Robinson Crusoe", "Book about man on island", 5,null, null));
-		datas.add(new Book(3L,"Robinson Crusoe", "Book about man on island", 3,null, null));
-		datas.add(new Book(4L,"Robinson Crusoe", "Book about man on island", 8,null, null));
-		given(bookRepository.saveAll(datas)).willAnswer(answer->answer.getArgument(0));
-		
-		List<Book> books = bookService.createBooks(datas); 
-		
-		assertEquals(books.size(), expected);
-	
-	}
-	
-	@Test
-	public void updateBook() {
-		Book book = new Book(1L,"Robinson Crusoe", "Book about man on island", 5, null, null);
-		
-		given(bookRepository.save(book)).willReturn(book);
-		
-		Book expected = bookService.updateBook(book);
-		
-		assertThat(expected).isNotNull();
-		
-		verify(bookRepository).save(any(Book.class));
-	}
-	
-	@Test
-	public void deleteBook() {
-		Long bookId = 1L;
-		
-		bookService.deleteBookById(bookId);
-		
-		verify(bookRepository, times(1)).deleteById(bookId);
-	}
-	
+        this.mockMvc.perform(get("/api/books/{id}",bookId))
+        	.andExpect(status().isOk())
+        	.andExpect(jsonPath("$.title", is(book.getTitle())))
+        	.andExpect(jsonPath("$.descr", is(book.getDescr())))
+        	.andExpect(jsonPath("$.reviews", is(book.getReviews())));
+        	
+    }
+
 }
